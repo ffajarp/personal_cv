b9eacc26f9b11078b58c9e4bc4da9128560f0925
fajarvrmn <abahripuh@gmail.com>
2026-08-27 21:09:58 +0700

    hide porto

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,7 +14,7 @@ export default function Home() {
       <About />
       <Experience />
       <Skills />
-      <Portfolio />
+      {/* <Portfolio /> */}
       <Education />
       <Certificates />
       <Contact />
